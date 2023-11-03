9522cfa3f08c46ebe7b2d956f9c7f8a6bca3af2a
Bryan <bryanborck@gmail.com>
2023-11-03 12:45:40 -0300

    style done in mybets

--- frontend/src/pages/MyBets/MyBets.js
+++ frontend/src/pages/MyBets/MyBets.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 export default function MyBet() {
     // const { myBetAcc } = useParams();
@@ -11,6 +11,8 @@ export default function MyBet() {
     // });
 
     let id = 1;
+
+    const history = useNavigate();
 
     const [myBets, setMyBets] = useState([
         // TESTE
@@ -72,20 +74,21 @@ export default function MyBet() {
 
     return (
         <div className="w-[75vw] p-12">
-            <h1 className="text-3xl text-white font-semibold mb-4">My Bets</h1>
+            <h1 className="text-3xl text-white font-semibold mb-8">My Bets</h1>
 
             {myBets.length ? (
-                <div className="flex flex-col bg-white border rounded-lg overflow-hidden">
-                <div className="flex bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+                <div className="flex flex-col shadow-lg rounded-lg overflow-hidden">
+                <div className="flex border-blue-color bg-gradient-to-r from-primary-color to-secondary-color rounded-lg border-[3px] text-white uppercase text-sm leading-normal">
                     <div className="py-3 px-6 text-left flex-1">Title</div>
                     <div className="py-3 px-6 text-left flex-1">Bet On</div>
                     <div className="py-3 px-6 text-left flex-1">Quantity</div>
                     <div className="py-3 px-6 text-left flex-1">Odd</div>
                     <div className="py-3 px-6 text-left flex-1">Status</div>
+                    <div className="py-3 px-6 text-left flex-1">Accept Bet</div>
                 </div>
-                <div className="text-gray-600 text-base font-light">
+                <div className="text-white text-base font-light">
                     {myBets.map((bet) => (
-                        <div key={bet.id} className="flex border-b border-gray-200 hover:bg-gray-100 items-center">
+                        <div key={bet.id} className="border-b h-14 items-center border-secondary-color flex hover:bg-secondary-color hover:bg-opacity-50">
                             <div className="py-3 px-6 text-left flex-1">{bet.title}</div>
                             <div className="py-3 px-6 text-left flex-1">{bet.betOn}</div>
                             <div className="py-3 px-6 text-left flex-1">{bet.quantity}</div>
@@ -93,6 +96,14 @@ export default function MyBet() {
                             <div className="py-3 px-6 text-left flex-1 relative">
                                 <div className={`h-4 w-4 rounded-full absolute left-[-10px] top-1/2 transform -translate-y-1/2 ${bet.status === "accepted" ? "bg-green-500" : bet.status === "pending" ? "bg-yellow-500" : "bg-red-500"}`}></div>
                                 {bet.status}
+                            </div>
+                            <div className="py-3 px-6 text-left flex-1">
+                                {bet.status === 'pending' && 
+                                    <button className="w-full bg-transparent hover:bg-blue-color text-clue-color font-bold hover:text-white py-2 px-4 border-2 border-blue-color hover:border-transparent rounded"
+                                            onClick={() => history('/')}>
+                                        Accept Bet
+                                    </button>
+                                }
                             </div>
                         </div>
                     ))}
